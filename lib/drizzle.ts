9bc2271094a03a7570ec17130dab27cf2d0f1c70
Joaquin <hola@jrltt.net>
2024-08-06 10:38:57 +0200

    feat(db): add schemas

--- lib/drizzle.ts
+++ lib/drizzle.ts
@@ -1,32 +1,72 @@
 import {
+  numeric,
+  pgEnum,
   pgTable,
   serial,
   text,
   timestamp,
   uniqueIndex,
-} from 'drizzle-orm/pg-core'
-import { InferSelectModel, InferInsertModel } from 'drizzle-orm'
-import { sql } from '@vercel/postgres'
-import { drizzle } from 'drizzle-orm/vercel-postgres'
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+import { InferSelectModel, InferInsertModel } from "drizzle-orm";
+import { sql } from "@vercel/postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
 
 export const UsersTable = pgTable(
-  'users',
+  "users",
   {
-    id: serial('id').primaryKey(),
-    name: text('name').notNull(),
-    email: text('email').notNull(),
-    image: text('image').notNull(),
-    createdAt: timestamp('createdAt').defaultNow().notNull(),
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    email: text("email").notNull(),
+    image: text("image").notNull(),
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
   },
   (users) => {
     return {
-      uniqueIdx: uniqueIndex('unique_idx').on(users.email),
-    }
+      uniqueIdx: uniqueIndex("unique_idx").on(users.email),
+    };
   }
-)
+);
 
-export type User = InferSelectModel<typeof UsersTable>
-export type NewUser = InferInsertModel<typeof UsersTable>
+export const OrderStatus = pgEnum("orderStatus", [
+  "draft",
+  "pending",
+  "progress",
+  "done",
+]);
+export const OrderTable = pgTable("order", {
+  id: uuid("id").primaryKey(),
+  userId: uuid("userId")
+    .references(() => UsersTable.id)
+    .notNull(),
+  amount: numeric("amount").notNull(),
+  status: OrderStatus("status").default("draft").notNull(),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+  updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+  completedAt: timestamp("completedAt"),
+});
+
+export const PartTable = pgTable("part", {
+  id: uuid("id").primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  description: varchar("description", { length: 255 }).notNull(),
+  category: varchar("category", { length: 255 }).notNull(), // TODO use enum or move to a new table
+});
+
+export const PartOrderTable = pgTable("partOrder", {
+  id: uuid("id").primaryKey(),
+  partId: uuid("partId")
+    .references(() => PartTable.id)
+    .notNull(),
+  orderId: uuid("orderId")
+    .references(() => OrderTable.id)
+    .notNull(),
+});
+
+// Types
+export type User = InferSelectModel<typeof UsersTable>;
+export type NewUser = InferInsertModel<typeof UsersTable>;
 
 // Connect to Vercel Postgres
-export const db = drizzle(sql)
+export const db = drizzle(sql);
