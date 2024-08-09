11fac3ea00676e27fcbb453397f8eab47ce45d8b
Joaquin <hola@jrltt.net>
2024-08-09 15:09:40 +0200

    chore: init shadcn ui

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,14 @@
-import ms from 'ms'
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import ms from "ms";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
 export const timeAgo = (timestamp: Date, timeOnly?: boolean): string => {
-  if (!timestamp) return 'never'
+  if (!timestamp) return "never";
   return `${ms(Date.now() - new Date(timestamp).getTime())}${
-    timeOnly ? '' : ' ago'
-  }`
-}
+    timeOnly ? "" : " ago"
+  }`;
+};
