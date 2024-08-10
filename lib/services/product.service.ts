36d9f83a88310b5cb8fd6fa1b84c817d8d8146c3
Joaquin <hola@jrltt.net>
2024-08-10 18:10:57 +0200

    feat(product service): add method w type

--- lib/services/product.service.ts
+++ lib/services/product.service.ts
@@ -1,4 +1,5 @@
 import { db } from "@/db/drizzle";
+import { ProductTypes } from "@/db/schema";
 
 // TODO refactor service and make it open to load different product types
 export async function getAllBicycle() {
@@ -7,14 +8,20 @@ export async function getAllBicycle() {
   });
 }
 
+export async function getProductDetail(id: string, type: ProductTypes) {
+  return await db.query.ProductTable.findFirst({
+    where: (products, { eq, and }) =>
+      and(eq(products.id, id), eq(products.productType, type)),
+    with: {
+      productPartOption: {
+        with: { partOption: true },
+      },
+    },
+  });
+}
+
 export async function getFullBicycle(id: string) {
   if (!id) throw new Error("Bicycle ID can not be null");
 
-  return await db.query.ProductTable.findFirst({
-    where: (products, { eq, and }) =>
-      and(eq(products.id, id), eq(products.productType, "bicycles")),
-    with: {
-      productPartOption: true,
-    },
-  });
+  return await getProductDetail(id, "bicycles");
 }
