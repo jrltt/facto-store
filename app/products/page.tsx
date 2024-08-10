b0aabbdbf6aaba096a20c9ac03f6a7ae580f98f8
Joaquin <hola@jrltt.net>
2024-08-10 18:11:43 +0200

    feat(public routes): add paths

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,7 @@
 "use server";
 
 import { getAllBicycle } from "@/lib/services/product.service";
+import Link from "next/link";
 
 export default async function Products() {
   const products = await getAllBicycle();
@@ -9,7 +10,10 @@ export default async function Products() {
     <>
       <h1>Products</h1>
       {products.map((product) => (
-        <div key={product.id}>Name: {product.name}</div>
+        <div key={product.id}>
+          Name: {product.name} / Detail:{" "}
+          <Link href={"/products/" + product.id}>click here</Link>
+        </div>
       ))}
     </>
   );
