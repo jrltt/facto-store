311a4a6a3017445fedf23dc6b103586a4ac3e587
Joaquin <hola@jrltt.net>
2024-08-10 18:14:08 +0200

    feat(part service): add query to get part with part options

--- lib/services/part.service.ts
+++ lib/services/part.service.ts
@@ -1,8 +1,37 @@
 import { db } from "@/db/drizzle";
+import { PartOptionTable, PartTable } from "@/db/schema";
+import { sql } from "drizzle-orm";
 
 // TODO add pagination
 export async function getParts() {
   return await db.query.PartTable.findMany();
+}
+
+export async function getPartsWithOptions() {
+  return await db
+    .select({
+      partId: PartTable.id,
+      partName: PartTable.name,
+      partDescription: PartTable.description,
+      partCategory: PartTable.category,
+      partOptions: sql`
+      COALESCE(
+        json_agg(
+          json_build_object(
+            'partOptionId', ${PartOptionTable.id},
+            'name', ${PartOptionTable.name},
+            'price', ${PartOptionTable.price},
+            'stock', ${PartOptionTable.stock},
+            'stockNumber', ${PartOptionTable.stockNumber}
+          )
+        ) FILTER (WHERE ${PartOptionTable.id} IS NOT NULL), '[]'
+      )
+    `.as("partOptions"),
+    })
+    .from(PartTable)
+    .leftJoin(PartOptionTable, sql`${PartTable.id} = ${PartOptionTable.partId}`)
+    .groupBy(PartTable.id)
+    .orderBy(PartTable.category);
 }
 
 // export async function getDetailPart(id: string) {}
