15fbb68fc77c4e0eb458f2b1bd5f732746508bff
Joaquin <hola@jrltt.net>
2024-08-11 16:41:16 +0200

    feat(product service): add queries

--- lib/services/product.service.ts
+++ lib/services/product.service.ts
@@ -1,6 +1,39 @@
 import { db } from "@/db/drizzle";
 import { ProductTypes } from "@/db/schema";
 
+// TODO add pagination
+export async function getProducts() {
+  return await db.query.ProductTable.findMany({
+    with: { productPartOption: true },
+    limit: 10,
+  });
+}
+
+export async function getProductsWithPartOptions() {
+  return await db.query.ProductTable.findMany({
+    with: {
+      productPartOption: {
+        with: {
+          partOption: {
+            columns: { id: true, name: true, price: true, stock: true },
+          },
+        },
+      },
+    },
+  });
+}
+
+export async function getProductPartOptions(productId: string) {
+  return await db.query.ProductPartOptionTable.findFirst({
+    where: (productPartOption, { eq }) =>
+      eq(productPartOption.productId, productId),
+    with: {
+      partOption: {
+        columns: { id: true, name: true, price: true, stock: true },
+      },
+    },
+  });
+}
 // TODO refactor service and make it open to load different product types
 export async function getAllBicycle() {
   return await db.query.ProductTable.findMany({
